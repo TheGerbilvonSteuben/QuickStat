7fc67750cdb67a5170c1e1454b4994563945126c
Jerret Stovall <Jerret.stovall19@gmail.com>
2022-02-22 11:17:53 -0700

    More seperation of UI and logic

--- QuickStat.py
+++ QuickStat.py
@@ -1,19 +1,9 @@
-# create a virtual environment in your current directory
-# python -m virtualenv kivy_venv
-
-#run this everytime in command.com
-# kivy_venv\Scripts\activate
-
-
 from cgitb import text
 import kivy
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.config import Config 
-
 from kivy.uix.gridlayout import GridLayout
-
-
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
 from kivy.lang import Builder
@@ -26,17 +16,13 @@ Window.size = (800,800)
 # Designate Our .kv design file 
 Builder.load_file('QuickStat.kv')
 
-class myLayout(GridLayout):
-  def __init__(self, **kwargs):
-    super(GridLayout, self).__init__(**kwargs)
-
+class myLayout(Widget):
     
   # Get the csv file
   def press(self):
     path = filechooser.open_file(title="Pick a CSV file..", 
                              filters=[("Comma-separated Values", "*.csv")])
     print(path)
-
 
       
 class QuickStat(App):
