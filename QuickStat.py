e7b6d443731e97f08b592227b561acc37e75a821
Ashish <ashishacharya0@gmail.com>
2022-02-22 01:10:13 -0700

    separated code and ui

--- QuickStat.py
+++ QuickStat.py
@@ -21,46 +21,27 @@ from kivy.uix.label import Label
 
 from plyer import filechooser
 
-
 # Set the app size
 Window.size = (800,800)
-
-# 0 being off 1 being on as in true / false 
-# you can use 0 or 1 && True or False 
-Config.set('graphics', 'resizable', True)
 
 # Full screen app
 #Window.fullscreen = True
 #Window.maximize()
 
-# # Designate Our .kv design file 
-# Builder.load_file('QuickStat.kv')
+# Designate Our .kv design file 
+Builder.load_file('QuickStat.kv')
 
-class GridLayout(GridLayout):
+# Need to check if we still need this
+class myLayout(GridLayout):
   def __init__(self, **kwargs):
     super(GridLayout, self).__init__(**kwargs)
 
-    # Need to set it as 1 for properly displaying output
-    self. cols = 1
-
-    # (+) Open File 
-    self.PLUS_BUTTON = Button(text="Open File", font_size = 32, pos = (20,20))
-    # bind the button
-    self.PLUS_BUTTON.bind(on_press = self.press)
-    # add the button
-    self.add_widget(self.PLUS_BUTTON)
-
-    # Footer
-    self.add_widget(Label(text='© QuickStat Team. All Rights Reserved.', font_size = 20))
-
-  def press(self, instance):
+    
+  # clickity clack action
+  def press(self):
     path = filechooser.open_file(title="Pick a CSV file..", 
                              filters=[("Comma-separated Values", "*.csv")])
     print(path)
-
-
-
-    
 
 
       
@@ -69,7 +50,7 @@ class Main(App):
   title = 'QuickStat'
   
   def build(self):
-    return GridLayout()
+    return myLayout()
 
 if __name__ == '__main__':
     Main().run()
