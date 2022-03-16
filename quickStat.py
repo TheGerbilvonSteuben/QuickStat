2fb8236bf6151d20a93bee2a34b257be60c16c69
Ashish <ashishacharya0@gmail.com>
2022-03-15 22:09:55 -0600

    removed leftover pylint comments, 2 button main window, 2nd button for git

--- quickStat.py
+++ quickStat.py
@@ -7,29 +7,26 @@ A app for retreiving statistics from a csv file.
 # activate virtual environment
 # kivy_venv\Scripts\activate
 
-
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.lang import Builder
-from plyer import filechooser
 from kivy.uix.screenmanager import ScreenManager, Screen
+from plyer import filechooser
 import pandas as pd
 
-
 # Set the app size
-Window.size = (400, 400)
+Window.size = (400, 260)
 
 # Global variables
 FILEPATH = ''
 DF = pd.DataFrame()
-# Declare Screens
 
 
-# Main window. Choose a dataset.
-class Startup(Screen):  # pylint: disable=too-few-public-methods
+# Main screen/window.
+class Startup(Screen):
     """Startup screen with buttons for csv file selection"""
     # Get the csv file
-    def file_chooser(self):  # pylint: disable=no-self-use
+    def file_chooser(self): 
         """Lets a user select a csv file"""
         global FILEPATH
         FILEPATH = filechooser.open_file(
@@ -38,36 +35,26 @@ class Startup(Screen):  # pylint: disable=too-few-public-methods
         print(FILEPATH)
 
 
-# Second window. Data Analysis Screen with several options
+# Data Analysis screen/window
 class Display(Screen):
     """Screen for displaying statistics from given csv file."""
     # Create dataframe from selected csv
-    def create_DF(self):  # pylint: disable=no-self-use
+    def create_df(self):
         """Function for creating a dataframe from given csv file."""
         global FILEPATH
         global DF
-        DF = pd.read_csv(FILEPATH[0])
-        print(DF.head)
+        df = pd.read_csv(FILEPATH[0])
+        print(df.head)
 
-        result = DF.select_dtypes(include='number')
+        result = df.select_dtypes(include='number')
         # print(result)
 
         numeric_cols = result.columns.values
         # print(numeric_cols)
 
         for col_name in numeric_cols:
-            print(col_name, " Mean:", DF[col_name].mean())
-        print(DF.describe())
-
-    # Checkbox exclude null
-    def exclude_null(self, instance, value):
-        """Function for excluding Null values."""
-        print("Exclude Null:", value)
-
-    # Checkbox exclude outliers
-    def exclude_outliers(self, instance, value):
-        """Function for excluding outliers."""
-        print("Exclude Outliers:", value)
+            print(col_name, " Mean:", df[col_name].mean())
+        print(df.describe())
 
 
 # Designate Our .kv design file
